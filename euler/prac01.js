2b2812f3e2952d2cc53c6d3ed26eeff1b30db28e
seo <riker7@naver.com>
2015-03-09 12:54:02 +0900

    euler/prac01.js add function

--- euler/prac01.js
+++ euler/prac01.js
@@ -3,3 +3,16 @@
 
 1000보다 작은 자연수 중에서 3 또는 5의 배수를 모두 더하면 얼마일까요?
 */
+addThreeFiveMultiple(1000);
+
+function addThreeFiveMultiple(baseline){
+	var sum = 0;
+	
+	for(var index = 1 ; index < baseline ; index++){
+		if(index % 3 == 0 || index % 5 == 0){
+			sum += index;
+		}
+	}
+
+	console.log("result : " + sum);
+}
