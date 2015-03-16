2a3f85d3508dcfef9ae0f3201b8f628829c8202c
DongJin <riker7@naver.com>
2015-03-16 14:42:15 +0900

    findPrime function create

--- euler/prac07.js
+++ euler/prac07.js
@@ -3,3 +3,27 @@
 
 이 때 10,001번째의 소수를 구하세요.
 */
+
+console.log(insertNumber(10001));
+
+function isPrime(value){
+	for(var index = 2 ; index < value; index++){
+		if(value % index == 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+function insertNumber(number){
+	var count = 1;
+	for(var index = 2 ; count <= number ; index++){
+		if(isPrime(index)){
+			if(count == number){
+				return index;
+			}
+			count++;
+			continue;
+		}
+	}
+}
