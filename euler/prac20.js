d6eccfaa962707c3a418d09831e98193ee2134d6
DongJin Seo <riker7@naver.com>
2015-03-29 23:59:52 +0900

    factorial function modify

--- euler/prac20.js
+++ euler/prac20.js
@@ -6,10 +6,12 @@ n! 이라는 표기법은 n × (n − 1) × ... × 3 × 2 × 1을 뜻합니다.
 
 100! 의 자리수를 모두 더하면 얼마입니까?
 */
+console.log(factorial(100));
 
 function factorial(maxIndex){
 	var result = 1;
 	for(var index = 1 ; index <= maxIndex; index++){
 		result *= index;
 	}
+	return result;
 }
