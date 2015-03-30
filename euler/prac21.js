9b8df0afebed9305f448590fe81e6875b2742c7c
DongJin Seo <riker7@naver.com>
2015-03-30 19:53:35 +0900

    function create

--- euler/prac21.js
+++ euler/prac21.js
@@ -9,3 +9,32 @@ a, b는 친화쌍이라 하고 a와 b를 각각 친화수(우애수)라고 합�
 
 10000 이하의 친화수들을 모두 찾아서 그 합을 구하세요.
 */
+
+console.log(gop()); 
+
+function gop(){
+	var result = 0;
+	var sum;
+	var temp;
+	for(var index = 1; index <= 10000 ; index++){
+		sum = sumDivideNumber(index);
+		if(index == sumDivideNumber(sum)){
+			console.log(sum + " " + index);
+			if(index == sum){
+				continue;
+			}
+			result += sum;
+		}
+	}
+	return result;
+}
+
+function sumDivideNumber(index){
+	var sum = 0;
+	for(var index2 = 1; index2 < index ; index2++){
+		if(index % index2 == 0){
+			sum += index2;
+		}
+	}
+	return sum;
+}
