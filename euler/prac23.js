2ae84a577534034056284fb329237f5a8ebe026d
DongJin Seo <riker7@naver.com>
2015-04-01 17:02:50 +0900

    function isOverNumber create

--- euler/prac23.js
+++ euler/prac23.js
@@ -11,3 +11,20 @@
 
 그렇다면, 초과수 두 개의 합으로 나타낼 수 없는 모든 양의 정수의 합은 얼마입니까?
 */
+console.log();
+
+function isOverNumber(number){
+	var sum = 0;
+	for(var index = 1; index < number ; index++){
+		if(number % index == 0){
+			sum += index;
+		}
+	}
+
+	if(sum > number){
+		return true;
+	} else{
+		return false;
+	}
+}
+
